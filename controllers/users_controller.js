f6dc92d3e14d3930e143eb40610375d82b4a7d78
unknown <udaymore437@gmail.com>
2023-03-24 18:40:10 +0530

    manual authentication

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,9 +1,26 @@
 const User= require('../models/user');
 
 module.exports.profile=function(req,res){
-    res.render('user_profile',{
-        title:"Profile"
-    })
+    if(req.cookies.user_id){
+        User.findById(req.cookies.user_id,function(err,user){
+            if(err){
+          console.log('No user_id found')
+            }
+            if(user){
+                return res.render('user_profile',{
+                    title:" User's Profile",
+                    user:user
+                })
+
+            }
+            return res.redirect('/users/signIn')
+        })
+    }else{
+        return res.redirect('/users/signIn');
+    }
+    // res.render('user_profile',{
+    //     title:"Profile"
+    // })
     // res.end('<h1>User Profile</h1>');
 }
 //singUp render
@@ -43,5 +60,45 @@ module.exports.create=function(req,res){
         }else{
             return res.redirect('back');
         }
+    })
+}
+
+module.exports.createSession= function(req,res){
+    //steps to authenticate user
+    //find the user
+
+    User.findOne({email:req.body.email},function(err,user){
+        if(err){
+            alert('user not found while signing in');
+            console.log('error in finding user while signing in');
+            return;
+        }
+        //handle user found
+        if(user){
+            //handle password which doesnt match
+            if(user.password != req.body.password){
+                return res.redirect('back');
+            }
+            //handle session creation
+            res.cookie('user_id',user.id);
+            return res.redirect('/users/profile');
+
+        }else{
+            //handle user not found
+            res.redirect('back');
+        }
+
+    })
+}
+
+//signout
+module.exports.signOut=function(req,res){
+    User.deleteOne({_id: req.cookies.user_id},function(err){
+        if(err){
+        console.log('error in deleting user_id');
+        return;
+        }
+        return res.redirect('/users/signIn');
+        
     })
 }
--- routes/users.js
+++ routes/users.js
@@ -11,5 +11,7 @@ router.get('/signUp', userController.signUp);
 router.get('/signIn',userController.signIn);
 
 router.post('/create',userController.create);
+router.post('/create-session',userController.createSession)
+router.post('/signout',userController.signOut);
 
 module.exports=router;
